aee7c60353c28cc6895773a946545e45bfa83156
Your Name <you@example.com>
2025-09-14 15:25:20 -0300

    fix(jest): add setupTests with jest-dom + polyfills, ignore Playwright tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,19 +1,6 @@
 import "@testing-library/jest-dom";
-import React from "react";
+import { TextEncoder, TextDecoder } from "util";
 
-// Polyfill ResizeObserver
-global.ResizeObserver = class ResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-} as any;
-
-// Mock ResponsiveContainer without JSX
-jest.mock("recharts", () => {
-  const Original = jest.requireActual("recharts");
-  return {
-    ...Original,
-    ResponsiveContainer: ({ children }: any) =>
-      React.createElement("div", { style: { width: "800px", height: "400px" } }, children),
-  };
-});
+// Polyfill for Node.js environment
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder as any;
