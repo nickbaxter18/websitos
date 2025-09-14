1b4977ab73715db83d84ea47c73496503e554081
Your Name <you@example.com>
2025-09-14 15:31:49 -0300

    fix(jest): use strict typings for TextEncoder/TextDecoder polyfills

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,6 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
 
-// Polyfill for Node.js environment
-(global as any).TextEncoder = TextEncoder;
-(global as any).TextDecoder = TextDecoder as any;
+// Polyfill for Node.js environment with proper typings
+(global as unknown as { TextEncoder: typeof TextEncoder }).TextEncoder = TextEncoder;
+(global as unknown as { TextDecoder: typeof TextDecoder }).TextDecoder = TextDecoder;
