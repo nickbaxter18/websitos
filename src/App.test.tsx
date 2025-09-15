b62f83a42465b1eaebf93765b584e228ef1c29aa
Your Name <you@example.com>
2025-09-15 07:53:13 -0300

    test: fix App.test import and update Status.tsx to use global types

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import App from "./App.js";
+import App from "./App";
 
 test("renders App shell without crashing", () => {
   render(<App />);
