4ccda7470db210f8465d9c6087b24c3a976ee434
Your Name <you@example.com>
2025-09-15 13:53:47 -0300

    chore(eslint): migrate ignores from .eslintignore to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,10 @@
-// eslint.config.js
-import tseslint from "typescript-eslint";
 import js from "@eslint/js";
-import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ["./tsconfig.eslint.json"],
-        tsconfigRootDir: process.cwd(),
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
+    ignores: ["babel.config.js", "jest.config.js"],
   },
 ];
