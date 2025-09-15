a4eea3b7d2d6de65c7bbee546b831ef0cec911bb
Your Name <you@example.com>
2025-09-15 08:42:09 -0300

    fix(tests): update Jest config and add TextEncoder/TextDecoder polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom";
+
+// Polyfill TextEncoder/TextDecoder for React Router and other libs in Jest
+import { TextEncoder, TextDecoder } from "util";
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
