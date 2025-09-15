80d3c36c14d625c82fce3373cd87b65581374c8c
Your Name <you@example.com>
2025-09-15 08:22:51 -0300

    chore: harden Jest + Husky config for stable ESM testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,1 @@
-/* eslint-env jest, node */
-
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "util";
-
-// Polyfill for Node.js environment with proper typings
-(global as unknown as { TextEncoder: typeof TextEncoder }).TextEncoder = TextEncoder;
-(global as unknown as { TextDecoder: typeof TextDecoder }).TextDecoder = TextDecoder;
+import "@testing-library/jest-dom/extend-expect";
