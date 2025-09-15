9d983be18d74d8ba576872181add7379c0ae98f2
Your Name <you@example.com>
2025-09-15 13:39:11 -0300

    fix(tests): enable React automatic runtime in Babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ export default {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
-    "@babel/preset-react"
+    ["@babel/preset-react", { runtime: "automatic" }]
   ]
 };
