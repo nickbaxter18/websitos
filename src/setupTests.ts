b2f6eee8e887800102d662b39020c912a079a315
Your Name <you@example.com>
2025-09-16 08:11:52 -0300

    fix(ts): use bundler moduleResolution + patch TextEncoder/TextDecoder in setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,11 +3,10 @@ import { TextEncoder, TextDecoder } from "util";
 
 declare global {
   // Augment NodeJS global type so TextEncoder/TextDecoder exist
-
   var TextEncoder: typeof TextEncoder;
-
   var TextDecoder: typeof TextDecoder;
 }
 
 global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+// Cast to any to bypass type incompatibility
+global.TextDecoder = TextDecoder as any;
